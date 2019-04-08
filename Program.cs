f33606fe2d981b07d14fa259df7456af71f7c168
Ben <kanobt61@gmail.com>
2019-04-08 19:12:54 -0400

    Add method to create random numbers
    We need random numbers between 0 and 1. The more decimal places the better.
    This seemed to be the easiest way to do it.
    
    NOTE: Random is limited by the size of int, so I could only go to 10
    decimal places. In contrast Ruby's rand seems to do 17 decimal places.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace PiSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Random rand = new Random();
+            decimal num;
+            num = NextRandom(rand);
+            Console.WriteLine(num);
+            num = NextRandom(rand);
+            Console.WriteLine(num);
+        }
+
+        static private decimal NextRandom(Random rand)
+        {
+            decimal big_number = 1_000_000_000;
+            decimal num = rand.Next(0, 1_000_000_000) / big_number;
+            return(num);
         }
     }
 }
