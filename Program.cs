3f856b5499c2c192a462984fad2fc91235c2056d
Ben <kanobt61@gmail.com>
2019-04-11 18:48:10 -0400

    Estimate Pi
    I added the code to run the simulation to estimate pi.
    
    The general idea is that we draw the unit circle and randomly select
    a point on the x,y axis. The ratio of points inside of the unit circle
    vs the points outside of the unit circle is pi.
    
    NOTE: here is a small performance gain by only using the first quadrant
    of the unit circle and multiplying the result by 4.

--- Program.cs
+++ Program.cs
@@ -6,19 +6,26 @@ namespace PiSharp
     {
         static void Main(string[] args)
         {
-            
+            int simulations = 100_000_000;
+            int inside_circle = 0;
+            double x;
+            double y;
             Random rand = new Random();
-            decimal num;
-            num = NextRandom(rand);
-            Console.WriteLine(num);
-            num = NextRandom(rand);
-            Console.WriteLine(num);
+
+            for (int i = 0; i < simulations; i++)
+            {
+                x = NextRandom(rand);
+                y = NextRandom(rand);
+                if (Math.Sqrt(x * x + y * y) < 1)
+                    inside_circle++;
+            }
+            Console.WriteLine(((double) inside_circle / simulations) * 4 );
         }
 
-        static private decimal NextRandom(Random rand)
+        static private double NextRandom(Random rand)
         {
-            decimal big_number = 1_000_000_000;
-            decimal num = rand.Next(0, 1_000_000_000) / big_number;
+            double big_number = 1_000_000_000;
+            double num = rand.Next(0, 1_000_000_000) / big_number;
             return(num);
         }
     }
